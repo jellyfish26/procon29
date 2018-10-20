2e60bff8c8e1ea20c49f46041b368f1b980baa85
jellyfish26 <jellyfish0162@gmail.com>
2018-10-20 15:29:01 +0900

    [add] Set the number of field titles

--- CompetititonField.cpp
+++ CompetititonField.cpp
@@ -7,35 +7,40 @@ using namespace std;
 class CompetitionField {
 private:
     int field[13][13];
+    bool myselfField[13][13] = {false};
+    bool enemyField[13][13] = {false};
+
     // pair<int, int> is pair<x coordinate, y coordinate>
     pair<int, int> myselfAgent1, myselfAgent2;
     pair<int, int> enemyAgent1, enemyAgent2;
     /*
      * When start this project. Initialization field size.
      */
-    int filedSizeX, filedSizeY;
+    int fieldSizeX, fieldSizeY;
 
-    void initializationAgentCoordinateSettingsDescriptions(string agent) {
+    void initializationAgentCoordinateSettingsDescriptions(const string &agent) {
         cout << "Please enter the location of " + agent + " . (x coordinate y coordinate)" << endl;
     }
 
 public:
-    bool initializationFieldSize(int x, int y) {
-        if (x > 0 && x <= 12 && y > 0 && y <= 12) {
-            filedSizeX = x;
-            filedSizeY = y;
-            cout << "[complete] Setting of filed size is completed." << endl;
-            return true;
-        } else {
-            cout << "[filed] Could not set field size. Please type it again." << endl;
-            return false;
+    void initializationFieldSize() {
+        while (true) {
+            cout << "Please enter the field size. (vertical length horizontal length)" << endl;
+            cin >> fieldSizeX >> fieldSizeY;
+            if (fieldSizeX > 0 && fieldSizeX <= 12 && fieldSizeY > 0 && fieldSizeY <= 12) {
+                cout << "[complete] Setting of filed size is completed." << endl;
+                return;
+            } else {
+                cout << "[filed] Could not set field size. Please type it again." << endl;
+            }
         }
     }
     /*
      * Since the explanation of "line symmetry in the horizontal direction or line symmetry in the vertical direction" is ambiguous,
      * ask the agent to input all the positions.
      */
-    bool initializationAganetCoordinateSettings() {
+    void initializationAgentCoordinateSettings() {
+        cout << fieldSizeX << " " << fieldSizeY << endl;
         initializationAgentCoordinateSettingsDescriptions("my first agent");
         cin >> myselfAgent1.first >> myselfAgent1.second;
         initializationAgentCoordinateSettingsDescriptions("my second agent");
@@ -46,5 +51,14 @@ public:
         cin >> enemyAgent2.first >> enemyAgent2.second;
     }
 
+    void initializationFieldTilesPointSeeings() {
+        cout << "Please enter the score of the panel." << endl;
+        cout << fieldSizeX << " " << fieldSizeY << endl;
+        for (int x = 0; x < fieldSizeX; ++x) {
+            for (int y = 0; y < fieldSizeY; ++y) {
+                cin >> field[x][y];
+            }
+        }
+    }
 
 };
--- main.cpp
+++ main.cpp
@@ -3,15 +3,12 @@
 using namespace std;
 
 int main() {
-    cin.tie(0);
+    cin.tie(nullptr);
     ios::sync_with_stdio(false);
     // Start project. Seeings Competition Filed Size.
-    pair<int, int> filedSize;
-    cout << "Please enter the field size. (vertical length horizontal length)" << endl;
-    cin >> filedSize.first >> filedSize.second;
-
     // The first argument is the vertical length, the second argument is the horizontal length.
-    CompetitionField().initializationFieldSize(filedSize.first, filedSize.second);
-    CompetitionField().initializationAganetCoordinateSettings();
-
+    auto competitionField = CompetitionField();
+    competitionField.initializationFieldSize();
+    competitionField.initializationAgentCoordinateSettings();
+    competitionField.initializationFieldTilesPointSeeings();
 }
